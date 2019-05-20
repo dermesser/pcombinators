6a9838eaa3072db77c807cd0241efc341cadf344
Lewin Bormann <lbo@spheniscida.de>
2019-05-21 01:18:21 +0200

    Comment on JSON parser.

--- pcombinators/json_test.py
+++ pcombinators/json_test.py
@@ -13,9 +13,13 @@ def JString():
     return Last(Skip(String('"')) + NoneInSet('"') + Skip(String('"')))
 
 def List():
-    wrapl = lambda l: [l] #if isinstance(l, list) else l
-    entry = (Skip(Whitespace()) + (Value()))
+    wrapl = lambda l: [l]
+    # An entry is any value.
+    entry = Value()
+    # A mid entry is a value followed by a comma.
     midentry = entry + Skip(String(','))
+    # A list is a [, followed by mid entries, followed by a final entry, and a
+    # closing ]. The list is wrapped in a list to prevent merging in other parsers.
     return (
             Skip(String('[')) +
             Repeat(midentry, -1) +
@@ -24,14 +28,30 @@ def List():
 
 def Dict():
     wrapl = lambda l: [l]
+    # A separator is whitespace, a colon, and more whitespace (Whitespace() also accepts empty string)
     separator = Skip((Whitespace() + String(":") + Whitespace()))
+    # Entry is a String followed by a separator and a value. Wrap the value in a list to prevent merging.
+    # The two-element list is converted to a tuple.
     entry = (JString() + separator + (Value() >> wrapl)) >> (lambda l: tuple(l))
+    # A mid entry is followed by a comma.
     midentry = entry + Skip(String(',') + Whitespace())
+    # A dict is a {, followed by entries, followed by a final entry, followed by a closing }
     dct = Skip(String("{")) + Repeat(midentry, -1) + entry + Skip(String("}"))
+    # Convert the list of tuples into a dict.
     fulldict = dct >> dict
     return fulldict
 
 class Value(Parser):
+    """Bare-bones, but fully functioning, JSON parser. Doesn't like escaped quotes.
 
+    Example:
+        >>> Value().parse(ParseState(my_json_string))
+        ({'id': 1.0,
+          'name': 'Foo',
+          'price': 123.0,
+          'tags': ['Bar', 'Eek'],
+          'stock': {'warehouse': 300.0, 'retail': 20.0}},
+         ParseState({"id":1,"name":"Foo","price":123,"tags":["Bar","Eek"],"stock":{"warehouse":300, "retail":20}}<>))
+    """
     def parse(self, st):
         return Last(Skip(Whitespace()) + (Dict() | List() | JString() | Float()) + Skip(Whitespace())).parse(st)
